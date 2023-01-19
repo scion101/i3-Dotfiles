eb43646b0a64073cc248374681bf39a87af9ec51
scion101 <122724012+scion101@users.noreply.github.com>
2023-01-19 08:58:27 +0000

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
+#Default Polybar launch script.....
+
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
+#polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 #polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+#echo "Bars launched..."
+
+# DT Polybar launch script.....
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do : ; done
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-i3 &
+done
 
 echo "Bars launched..."
